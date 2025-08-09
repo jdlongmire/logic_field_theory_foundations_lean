f7d3d75c3dd877782661e105678d9188a843ad77
JAMES (JD) Longmire <Longmire.jd@gmail.com>
2025-08-09 09:09:33 +0000

    Normalize to lakefile.lean and pin content

--- Lakefile.lean
+++
@@ -1,9 +0,0 @@
-import Lake
-open Lake DSL
-
-package lft
-
-lean_lib LFT
-
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
--- lakefile.lean
+++ lakefile.lean
@@ -1,14 +1,9 @@
 import Lake
 open Lake DSL
 
-package LFT where
-  leanOptions := #[
-    ⟨`pp.unicode.fun, true⟩
-  ]
+package lft
+
+lean_lib LFT
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
-
-@[default_target]
-lean_lib «LFT» where
-  -- add library configuration options here
